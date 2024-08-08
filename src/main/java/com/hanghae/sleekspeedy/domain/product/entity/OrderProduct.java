7f0708ac389443eae8989b2bfd632642dc848af8
beunchoi <byungeun0228@gmail.com>
2024-08-08 17:10:19 +0900

    feat : entity 생성

---
+++ src/main/java/com/hanghae/sleekspeedy/domain/product/entity/OrderProduct.java
@@ -0,0 +1,36 @@
+package com.hanghae.sleekspeedy.domain.product.entity;
+
+import com.hanghae.sleekspeedy.domain.order.entity.Order;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "order_product")
+@NoArgsConstructor
+public class OrderProduct {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id", nullable = false)
+  private Order order;
+
+  @Column(nullable = false)
+  private int count;
+}
---
+++ src/main/java/com/hanghae/sleekspeedy/domain/product/entity/Product.java
@@ -0,0 +1,33 @@
+package com.hanghae.sleekspeedy.domain.product.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "product")
+@NoArgsConstructor
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String image;
+
+  @Column(nullable = false)
+  private int price;
+
+  @Column(nullable = false)
+  private String category;
+}
