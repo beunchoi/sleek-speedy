a77b7173dc6757a4ca12b099e05a6d95d52c95e2
beunchoi <byungeun0228@gmail.com>
2024-08-08 16:08:09 +0900

    init commit

---
+++ src/main/java/com/hanghae/sleekspeedy/SleekSpeedyApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.sleekspeedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleekSpeedyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SleekSpeedyApplication.class, args);
+  }
+
+}
