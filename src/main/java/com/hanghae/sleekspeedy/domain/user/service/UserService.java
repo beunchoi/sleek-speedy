c46ab79202c7601bad8b74df8a3852ab645d3d04
beunchoi <byungeun0228@gmail.com>
2024-08-13 23:05:21 +0900

    feat : 유저 권한 생성

--- src/main/java/com/hanghae/sleekspeedy/domain/user/service/UserService.java
+++ src/main/java/com/hanghae/sleekspeedy/domain/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.hanghae.sleekspeedy.domain.basket.entity.Basket;
 import com.hanghae.sleekspeedy.domain.basket.repository.BasketRepository;
 import com.hanghae.sleekspeedy.domain.user.dto.SignupRequestDto;
 import com.hanghae.sleekspeedy.domain.user.entity.User;
+import com.hanghae.sleekspeedy.domain.user.entity.UserRoleEnum;
 import com.hanghae.sleekspeedy.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,8 @@ public class UserService {
   private final PasswordEncoder passwordEncoder;
   private final UserRepository userRepository;
   private final BasketRepository basketRepository;
+
+  private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
 
   public void signup(SignupRequestDto request) {
     String username = request.getUsername();
@@ -30,7 +33,12 @@ public class UserService {
       throw new IllegalArgumentException("중복된 email 입니다.");
     }
 
-    User user = new User(request, password);
+    UserRoleEnum role = UserRoleEnum.USER;
+    if (ADMIN_TOKEN.equals(request.getAdminToken())) {
+      role = UserRoleEnum.ADMIN;
+    }
+
+    User user = new User(request, password, role);
     userRepository.save(user);
     basketRepository.save(new Basket(user));
   }
